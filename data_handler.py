38dbe62ec6cf0731f9f82462d6377b1b56712c1e
PrzemekKomorowski <106514184+PrzemekKomorowski@users.noreply.github.com>
2022-09-07 20:20:45 +0200

    Przekopiowanie nowych zmian

--- data_handler.py
+++ data_handler.py
@@ -1,24 +1,27 @@
+from datetime import datetime
 import uuid
 import csv
 import os
-DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'questions.csv'
-DATA_HEADER = ['title', 'question']
-
-
+dir_path = os.path.dirname(os.path.realpath(__file__))
+DATA_HEADER = ['id', 'date', 'title', 'question']
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 def get_unique_id():
     id = uuid.uuid4()
     return id
 
 
-def save_data(title, user_question):
-    data_list = {'title': title,
+def save_data(id, title, user_question):
+    data_list = {'id': id,
+                 'date': dt_string,
+                 'title': title,
                  'question': user_question}
-    with open('questions.csv', "a") as csv_file:
+    with open(f"{dir_path}/sample_data/question.csv", "a") as csv_file:
         dict_object = csv.DictWriter(csv_file, fieldnames=DATA_HEADER)
         dict_object.writerow(data_list)
 
 
 def get_all_questions():
-    with open('questions.csv') as csv_file:
+    with open(f"{dir_path}/sample_data/question.csv") as csv_file:
         reader = csv.reader(csv_file)
         return list(reader)
--- server.py
+++ server.py
@@ -4,14 +4,20 @@ import data_handler
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
-def main_page():
+@app.route("/list", methods=['GET'])
+def list_page():
     return render_template('list.html')
 
+@app.route("/", methods=['GET'])
+def main_page():
+    return render_template('main.html')
 
-@app.route('/question')
-def question():
-    pass
+@app.route('/question/<id>')
+def question(id):
+    question_list = data_handler.get_all_questions()
+    for i in question_list:
+        if id in i:
+            return render_template('question.html', user_question=i)
 
 
 @app.route('/add-question')
@@ -21,17 +27,25 @@ def add_question():
 
 @app.route('/add-question', methods=['POST'])
 def get_data():
-
+    unique_id = data_handler.get_unique_id()
     title = request.form['title']
     user_question = request.form['question']
 
-    data_handler.save_data(title, user_question)
-    return redirect(url_for('main_page'))
+    data_handler.save_data(unique_id, title, user_question)
+    return redirect(url_for('question', id=unique_id))
+
+@app.route('/question-added', methods=['POST'])
+def get_data_2():
+    unique_id = data_handler.get_unique_id()
+    anwser = request.form['anwser']
+
+    data_handler.save_data(unique_id,anwser)
+    return redirect(url_for('anwser', id=unique_id))
 
 
 if __name__ == "__main__":
     app.run(
         host='0.0.0.0',
-        port=9000,
+        port=5000,
         debug=True,
     )
